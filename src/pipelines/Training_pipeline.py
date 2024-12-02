db25ec2da4c1515ebf19bbae196a85fa04acc1ef
shubhamprajapati241 <shubhamprajapati241@gmail.com>
2024-12-02 07:30:45 +0530

    added-pipelines

--- src/pipelines/Training_pipeline.py
+++ src/pipelines/Training_pipeline.py
@@ -1,7 +1,7 @@
 from src.components.Data_ingestion import DataIngestion
 from src.components.Data_transformation import DataTransformation
 from src.components.Model_trainer import ModelTrainer
-from src.components.Model_evaluation import ModelEvaluation
+# from src.components.Model_evaluation import ModelEvaluation
 
 obj=DataIngestion()
 train_data_path,test_data_path=obj.initiate_data_ingestion()
@@ -12,5 +12,5 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
 
-model_eval_obj = ModelEvaluation()
-model_eval_obj.initiate_model_evaluation(test_arr)
+# model_eval_obj = ModelEvaluation()
+# model_eval_obj.initiate_model_evaluation(test_arr)
